42718064e0b2583172aed72c01cc185a880e9928
wuwenjun <wuwenjun@sinosoft.com.cn>
2023-02-22 00:38:19 +0800

    测试

--- src/test/java/SelectionSort.java
+++ src/test/java/SelectionSort.java
@@ -21,6 +21,7 @@ public class SelectionSort {
         System.out.println("结果："+ Arrays.toString(result));
     }
 
+
     public int[] selection(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
 
